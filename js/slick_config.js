1d28bf746817a4b74cc8d32e813473e8fbb88c39
Thomasvdv31 <r0379766@student.thomasmore.be>
2023-10-30 22:04:06 +0100

    Moved scripts and external plugins
    - slick scripts are now in the js folder
    - external plugins are now in hte plugin folder
    
    - Changed the arrow position of the hobby carousel

---
+++ js/slick_config.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.slick-carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000, // Autoplay speed in milliseconds (2 seconds in this example)
+    });
+});
